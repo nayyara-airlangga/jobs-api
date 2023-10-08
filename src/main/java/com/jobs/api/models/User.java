f704670425a318a0c5a62923c9cc0359c67c3604
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-08 19:22:34 +0700

    feat: create user model

---
+++ src/main/java/com/jobs/api/models/User.java
@@ -0,0 +1,65 @@
+package com.jobs.api.models;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(name = "password_hash")
+    private String password;
+
+    @Column(columnDefinition = "BOOLEAN DEFAULT TRUE")
+    private boolean active;
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isEnabled();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + this.toString()));
+
+        return grantedAuthorities;
+    }
+
+}
