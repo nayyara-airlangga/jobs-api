a92af045dccee16225f9843112134cc9ffa05740
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-10-08 14:57:29 +0700

    feat: add job model

---
+++ src/main/java/com/jobs/api/models/Job.java
@@ -0,0 +1,34 @@
+package com.jobs.api.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class Job {
+    private String id;
+
+    private String type;
+
+    private String url;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    private String company;
+
+    @JsonProperty("company_url")
+    private String companyUrl;
+
+    private String location;
+
+    private String title;
+
+    private String description;
+
+    @JsonProperty("how_to_apply")
+    private String howToApply;
+
+    @JsonProperty("company_logo")
+    private String companyLogo;
+}
